73dfd206ffcbb2763629dc0f26c70b3ffad9e56c
Kevin Little <127446342+littlekt1@users.noreply.github.com>
2023-10-13 09:37:55 -0400

    Update HorrorGame.java
     Added third option

--- HorrorGame.java
+++ HorrorGame.java
@@ -6,6 +6,8 @@ public class HorrorGame {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
+        boolean hasKey = false;
+        int health = 100; // Initialize player's health
 
         System.out.println("Welcome to the Horror Game!");
         System.out.print("Enter your name: ");
@@ -14,18 +16,18 @@ public class HorrorGame {
         System.out.println("You find yourself in a dark, creepy mansion at night.");
         System.out.println("You must find a way to escape before something terrible happens.");
 
-        boolean hasKey = false;
-
-        while (true) {
+        while (health > 0) {
             System.out.println("\nYou are in the main hallway. What do you want to do?");
-            System.out.println("1. Explore the mansion");
-            System.out.println("2. Try to exit the mansion");
+            System.out.println("1. Explore the mansion, searching a random room.");
+            System.out.println("2. Try to exit the mansion through the spooky front door.");
+            System.out.println("3. Take off your shoes, let the dogs out (your toes) and go sit on the dusty couch and watch tv.");
 
             int choice = scanner.nextInt();
 
             if (choice == 1) {
-                hasKey = exploreRoom(random, hasKey);
+                hasKey = exploreRoom(random, hasKey, health);
                 if (random.nextDouble() < 0.2) {
+                    health -= 20; // Losing health during a scare event
                     scare();
                 }
             } else if (choice == 2) {
@@ -33,18 +35,30 @@ public class HorrorGame {
                     System.out.println("\nCongratulations, " + playerName + "! You've escaped the haunted mansion!");
                     break;
                 } else {
-                    System.out.println("\nYou try to exit the mansion, but the door is locked.");
+                    System.out.println("\nYou try to exit the mansion, but the door is locked. You hear a spooky laugh in the distance.");
                     System.out.println("You need to find a key to unlock it.");
                 }
+            } else if (choice == 3) {
+                health -= 100; // Losing health during a scare event
+                System.out.println("\nThe ghost eats your toes! And he doesn't have cable! YOU DIED!");
+                break; // End the game immediately when the player chooses option 3
             }
+
+            System.out.println("Your health: " + health);
         }
+
+        if (health <= 0) {
+            System.out.println("You've succumbed to the horrors of the mansion. Game over!");
+        }
+
 
         scanner.close();
     }
 
-    public static boolean exploreRoom(Random random, boolean hasKey) {
+    public static boolean exploreRoom(Random random, boolean hasKey, int health) {
         System.out.println("\nYou enter a dark and spooky room.");
         if (random.nextDouble() < 0.2) {
+            health -= 30; // Losing health during a scare event
             scare();
         } else {
             System.out.println("You find an old key while exploring.");
